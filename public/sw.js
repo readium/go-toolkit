b6b24adef1460684b17aab368a2a1a14c5e8527e
Hadrien Gardeur <hadrien.gardeur@feedbooks.com>
2016-10-17 16:42:15 +0200

    Changed to cache then network

--- public/sw.js
+++ public/sw.js
@@ -20,15 +20,12 @@ self.addEventListener('activate', event => {
   clients.claim();
 });
 
-/*
-For a publication, it seems better to do network then cache than the opposite.
-Could be problematic when the network is very slow, but has the benefit of being fresh.
-*/
+/* For EPUB files that are streamed, cache then network makes the most sense. */
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 
