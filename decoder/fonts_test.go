eb29528938a25baba15107dca1971ed636b28678
Bastien Quelen <bastien.quelen@feedbooks.com>
2017-04-06 17:24:58 +0200

    fix fonts test from import cycle

--- decoder/fonts_test.go
+++ decoder/fonts_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/readium/r2-streamer-go/models"
-	"github.com/readium/r2-streamer-go/parser"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -16,7 +15,8 @@ var testFonts []byte
 
 func init() {
 
-	testPublication, _ = parser.Parse("../test/readium-test-files/functional/smoke-tests/SmokeTestFXL")
+	//	testPublication, _ = parser.Parse("../test/readium-test-files/functional/smoke-tests/SmokeTestFXL")
+	testPublication.Metadata.Identifier = "urn:uuid:36d5078e-ff7d-468e-a5f3-f47c14b91f2f"
 	ft, _ := os.Open("../test/readium-test-files/functional/smoke-tests/SmokeTestFXL/fonts/cut-cut.woff")
 	testFonts, _ = ioutil.ReadAll(ft)
 }
@@ -26,7 +26,7 @@ func TestAdobeFonts(t *testing.T) {
 	f, _ := os.Open("../test/readium-test-files/functional/smoke-tests/SmokeTestFXL/fonts/cut-cut.adb.woff")
 
 	Convey("Given cut-cut.adb.woff fonts", t, func() {
-		fd, _ := DecodeAdobeFont(testPublication, models.Link{}, f)
+		fd, _ := DecodeAdobeFont(&testPublication, models.Link{}, f)
 		buff, _ := ioutil.ReadAll(fd)
 		Convey("The adobe fonts is deobfuscated", func() {
 			So(bytes.Equal(buff, testFonts), ShouldBeTrue)
@@ -41,7 +41,7 @@ func TestIdpfFonts(t *testing.T) {
 	f, _ := os.Open("../test/readium-test-files/functional/smoke-tests/SmokeTestFXL/fonts/cut-cut.obf.woff")
 
 	Convey("Given cut-cut.obf.woff fonts", t, func() {
-		fd, _ := DecodeIdpfFont(testPublication, models.Link{}, f)
+		fd, _ := DecodeIdpfFont(&testPublication, models.Link{}, f)
 		buff, _ := ioutil.ReadAll(fd)
 		Convey("The idpf fonts is deobfuscated", func() {
 			So(bytes.Equal(buff, testFonts), ShouldBeTrue)
