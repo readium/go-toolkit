8307e69e30307878817dba64968092ebb75345b3
Bastien Quelen <bastien.quelen@feedbooks.com>
2017-05-03 16:40:50 +0200

    related #37, change some name and fields

--- models/opds2/opds2.go
+++ models/opds2/opds2.go
@@ -6,7 +6,7 @@ import (
 	"github.com/readium/r2-streamer-go/models"
 )
 
-// OPDSFeed represent the root opds2 feed
+// OPDSFeed is a collection as defined in Readium Web Publication Manifest
 type OPDSFeed struct {
 	Context      []string             `json:"@context"`
 	Metadata     OPDSMetadata         `json:"metadata"`
@@ -14,25 +14,25 @@ type OPDSFeed struct {
 	Publications []models.Publication `json:"publications,omitempty"`
 	Navigation   []models.Link        `json:"navigation,omitempty"`
 	Facets       []OPDSFacet          `json:"facets,omitempty"`
-	Groups       []OPDSCollection     `json:"groups,omitempty"`
-	Modified     *time.Time           `json:"modified,omitempty"`
+	Groups       []OPDSGroup          `json:"groups,omitempty"`
 }
 
-// OPDSMetadata information for the feed, facets or collections
+// OPDSMetadata has a limited subset of metadata compared to a publication
 type OPDSMetadata struct {
-	RDFType       string `json:"@type,omitempty"`
-	Title         string `json:"title"`
-	NumberOfItems int    `json:"numberOfItems,omitempty"`
+	RDFType       string     `json:"@type,omitempty"`
+	Title         string     `json:"title"`
+	NumberOfItems int        `json:"numberOfItems,omitempty"`
+	Modified      *time.Time `json:"modified,omitempty"`
 }
 
-// OPDSFacet facet information for filtering
+// OPDSFacet is a collection that contains a facet group
 type OPDSFacet struct {
 	Metadata OPDSMetadata  `json:"metadata"`
 	Links    []models.Link `json:"links"`
 }
 
-// OPDSCollection collection of publication
-type OPDSCollection struct {
+// OPDSGroup is a group collection that must contain publications
+type OPDSGroup struct {
 	Metadata     OPDSMetadata         `json:"metadata"`
 	Publications []models.Publication `json:"publications"`
 	Links        []models.Link        `json:"links"`
