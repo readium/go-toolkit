38fd0fb74375784082199b7b769a7f77159a33bf
Henry <chocolatkey@gmail.com>
2021-07-30 00:46:19 -0700

    update contributor with custom json marshal

--- pkg/pub/contributor.go
+++ pkg/pub/contributor.go
@@ -1,5 +1,10 @@
 package pub
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 // Contributor
 // https://github.com/readium/webpub-manifest/tree/master/contexts/default#contributors
 // https://readium.org/webpub-manifest/schema/contributor-object.schema.json
@@ -8,7 +13,7 @@ type Contributor struct {
 	LocalizedSortAs *LocalizedString `json:"sortAs,omitempty"`         // The string used to sort the name of the contributor.
 	Identifier      string           `json:"identifier,omitempty"`     // An unambiguous reference to this contributor.
 	Roles           string           `json:"role,omitempty"`           // The roles of the contributor in the publication making.
-	Position        *float64         `json:"position"`                 // The position of the publication in this collection/series, when the contributor represents a collection. TODO validator
+	Position        *float64         `json:"position,omitempty"`       // The position of the publication in this collection/series, when the contributor represents a collection. TODO validator
 	Links           []Link           `json:"links,omitempty"`          // Used to retrieve similar publications for the given contributor.
 }
 
@@ -21,4 +26,31 @@ func (c Contributor) SortAs() string {
 		return ""
 	}
 	return c.LocalizedSortAs.String()
+}
+
+func (c Contributor) MarshalJSON() ([]byte, error) {
+	if c.LocalizedSortAs == nil && c.Identifier == "" && c.Roles == "" && c.Position == nil && c.Links == nil && len(c.LocalizedName.translations) == 1 {
+		// If everything but name is empty, and there's just one name, contributor can be just a name
+		return json.Marshal(c.LocalizedName)
+	}
+	return json.Marshal(c)
+}
+
+func (c *Contributor) UnmarshalJSON(data []byte) error {
+	var d interface{}
+	err := json.Unmarshal(data, &d)
+	if err != nil {
+		return err
+	}
+	switch d.(type) {
+	case string: // Just a single string Contributor
+		c.LocalizedName = NewLocalizedStringFromString(d.(string))
+	case map[string]interface{}: // Actual object Contributor
+		type CNT *Contributor // Prevent infinite recursion
+		cnt := CNT(c)
+		return json.Unmarshal(data, cnt)
+	default:
+		return errors.New("Contributor has invalid JSON object")
+	}
+	return nil
 }
